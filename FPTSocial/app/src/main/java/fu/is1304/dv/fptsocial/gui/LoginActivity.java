ea3e9337d7e94cfa554a42349afec157df3fb1c8
vankcdhv <vank48dhv@gmail.com>
2020-06-23 10:12:50 +0700

    VanLT6 - update

--- FPTSocial/app/src/main/java/fu/is1304/dv/fptsocial/gui/LoginActivity.java
+++ FPTSocial/app/src/main/java/fu/is1304/dv/fptsocial/gui/LoginActivity.java
@@ -2,7 +2,9 @@ package fu.is1304.dv.fptsocial.gui;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.AsyncListUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -11,48 +13,63 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import fu.is1304.dv.fptsocial.R;
-import fu.is1304.dv.fptsocial.common.Const;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText txtEmail;
+    private EditText txtPassword;
     private FirebaseAuth firebaseAuth;
-    private EditText txtUsername, txtPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        init();
+
+        initComponents();
     }
 
-    public void init(){
-        firebaseAuth = FirebaseAuth.getInstance();
-        txtUsername = findViewById(R.id.txtUsername);
+    private void initComponents() {
+        txtEmail = findViewById(R.id.txtEmail);
         txtPassword = findViewById(R.id.txtPassword);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() != null) {
+            loginComplete();
+        }
     }
 
-    public void btnLogin(View v){
-        String email = txtUsername.getText().toString();
+    public void btnLoginOnClick(View view) {
+        String email = txtEmail.getText().toString();
         String password = txtPassword.getText().toString();
-        if(!email.isEmpty() && !password.isEmpty()){
-        firebaseAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<com.google.firebase.auth.AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<com.google.firebase.auth.AuthResult> task) {
-                        //getResources();
-                        Toast.makeText(LoginActivity.this, "Thành công", Toast.LENGTH_LONG).show();
-                        setResult(Const.RESULT_CODE_COMPLETE);
-                        finish();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(LoginActivity.this, "Thất bại", Toast.LENGTH_LONG).show();
-                    }
-                });
+
+        if (!email.isEmpty() && !password.isEmpty()) {
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                loginComplete();
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Email và mật khẩu không đúng", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
+        } else {
+            Toast.makeText(this, "Email và mật khẩu không được để trống", Toast.LENGTH_SHORT).show();
         }
+    }
+
+
+
+    private void loginComplete() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 
--- FPTSocial/app/src/main/java/fu/is1304/dv/fptsocial/gui/MainActivity.java
+++ FPTSocial/app/src/main/java/fu/is1304/dv/fptsocial/gui/MainActivity.java
@@ -1,75 +1,58 @@
 package fu.is1304.dv.fptsocial.gui;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import fu.is1304.dv.fptsocial.R;
-import fu.is1304.dv.fptsocial.common.Const;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
-    private TextView labelEmail;
-
+    private EditText txtName, txtAge;
+    private DatabaseReference dbReference;
+    private FirebaseDatabase firebaseDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        requestPermissions();
-        init();
-    }
+        initComponents();
 
-    private void init(){
-        labelEmail = findViewById(R.id.labelEmail);
+    }
+    private void initComponents() {
         firebaseAuth = FirebaseAuth.getInstance();
-        setUserInfo();
-    }
-
-    public void logout(View v){
-        firebaseAuth.signOut();
-        init();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode){
-            case Const.REQUEST_CODE_LOGIN:
-                setUserInfo();
-                break;
-        }
-    }
-
-    private void setUserInfo(){
         firebaseUser = firebaseAuth.getCurrentUser();
-        if (firebaseUser==null){
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivityForResult(intent, Const.REQUEST_CODE_LOGIN);
-        } else {
-            labelEmail.setText(firebaseUser.getEmail());
-        }
+        if(firebaseUser==null) openLoginActivity();
+        txtName = findViewById(R.id.txtName);
+        txtAge = findViewById(R.id.txtPassword);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        dbReference = firebaseDatabase.getReference("users");
     }
 
-
-    private void requestPermissions() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
-            return;
-        } else if (shouldShowRequestPermissionRationale(Manifest.permission.INTERNET)) {
-
-        } else {
-            requestPermissions(new String[]{Manifest.permission.INTERNET}, 1000);
-        }
+    public void btnLogoutClick(View v){
+        firebaseAuth.signOut();
+        openLoginActivity();
     }
+
+    private void openLoginActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void btnAddOnClick(View v){
+        String name = txtName.getText().toString();
+        int age = Integer.parseInt(txtAge.getText().toString().trim());
+
+    }
+
 }
