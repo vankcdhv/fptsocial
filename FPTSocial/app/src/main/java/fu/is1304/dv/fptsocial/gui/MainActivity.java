bbcd9fa7ff13e77fbb23a7c880e28d72e77ef78c
vankcdhv <vank48dhv@gmail.com>
2020-06-23 16:56:36 +0700

    VanLT6 - Update function read data

--- FPTSocial/app/src/main/java/fu/is1304/dv/fptsocial/gui/MainActivity.java
+++ FPTSocial/app/src/main/java/fu/is1304/dv/fptsocial/gui/MainActivity.java
@@ -1,5 +1,6 @@
 package fu.is1304.dv.fptsocial.gui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,10 +11,18 @@ import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import fu.is1304.dv.fptsocial.R;
+import fu.is1304.dv.fptsocial.entity.User;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
@@ -33,10 +42,28 @@ public class MainActivity extends AppCompatActivity {
         firebaseUser = firebaseAuth.getCurrentUser();
         if(firebaseUser==null) openLoginActivity();
         txtName = findViewById(R.id.txtName);
-        txtAge = findViewById(R.id.txtPassword);
+        txtAge = findViewById(R.id.txtAge);
         firebaseDatabase = FirebaseDatabase.getInstance();
         dbReference = firebaseDatabase.getReference("users");
+        dbReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot data : snapshot.getChildren()){
+                    String key = data.getKey();
+                    Gson gson = new Gson();
+                    User user = gson.fromJson(gson.toJson(data.getValue()) , User.class);
+                    txtName.setText(user.getName());
+                    txtAge.setText(user.getAge()+"");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
+
 
     public void btnLogoutClick(View v){
         firebaseAuth.signOut();
@@ -52,7 +79,8 @@ public class MainActivity extends AppCompatActivity {
     public void btnAddOnClick(View v){
         String name = txtName.getText().toString();
         int age = Integer.parseInt(txtAge.getText().toString().trim());
-
+        User user = new User(name, age);
+        dbReference.child("users").child(firebaseUser.getUid()).setValue(user);
     }
 
 }
