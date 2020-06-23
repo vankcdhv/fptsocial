59cffe1ae8e8855fbfb665a0e69a7723e97b50a9
vankcdhv <vank48dhv@gmail.com>
2020-06-23 22:48:19 +0700

    VanLT6 - update

--- FPTSocial/app/src/main/java/fu/is1304/dv/fptsocial/gui/MainActivity.java
+++ FPTSocial/app/src/main/java/fu/is1304/dv/fptsocial/gui/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -16,10 +20,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 import fu.is1304.dv.fptsocial.R;
 import fu.is1304.dv.fptsocial.entity.User;
@@ -27,9 +30,15 @@ import fu.is1304.dv.fptsocial.entity.User;
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
-    private EditText txtName, txtAge;
     private DatabaseReference dbReference;
     private FirebaseDatabase firebaseDatabase;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference storageReference;
+
+
+    private EditText txtName, txtAge;
+    private ImageView imgTest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,24 +46,34 @@ public class MainActivity extends AppCompatActivity {
         initComponents();
 
     }
+
     private void initComponents() {
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
-        if(firebaseUser==null) openLoginActivity();
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        dbReference = firebaseDatabase.getReference();
+
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+
+        if (firebaseUser == null) openLoginActivity();
+
+        imgTest = findViewById(R.id.imgTest);
         txtName = findViewById(R.id.txtName);
         txtAge = findViewById(R.id.txtAge);
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        dbReference = firebaseDatabase.getReference("users");
-        dbReference.addValueEventListener(new ValueEventListener() {
+
+
+        dbReference.child("users").child(firebaseUser.getUid()).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot data : snapshot.getChildren()){
-                    String key = data.getKey();
-                    Gson gson = new Gson();
-                    User user = gson.fromJson(gson.toJson(data.getValue()) , User.class);
-                    txtName.setText(user.getName());
-                    txtAge.setText(user.getAge()+"");
-                }
+
+                String key = snapshot.getKey();
+                Gson gson = new Gson();
+                User user = gson.fromJson(gson.toJson(snapshot.getValue()), User.class);
+                txtName.setText(user.getName());
+                txtAge.setText(user.getAge() + "");
+
             }
 
             @Override
@@ -62,21 +81,31 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        storageReference.child("images/4.JPG").getBytes(1024 * 1024 * 20)
+                .addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                    @Override
+                    public void onSuccess(byte[] bytes) {
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+
+                        imgTest.setImageBitmap(bitmap);
+                    }
+                });
     }
 
 
-    public void btnLogoutClick(View v){
+    public void btnLogoutClick(View v) {
         firebaseAuth.signOut();
         openLoginActivity();
     }
 
-    private void openLoginActivity(){
+    private void openLoginActivity() {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
     }
 
-    public void btnAddOnClick(View v){
+    public void btnAddOnClick(View v) {
         String name = txtName.getText().toString();
         int age = Integer.parseInt(txtAge.getText().toString().trim());
         User user = new User(name, age);
